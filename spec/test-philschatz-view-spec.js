9223276ad286e0635b947e7d3681ebef5f389025
Philip Schatz <phil@cnx.org>
2017-12-02 21:06:17 -0600

    Initial commit

---
+++ spec/test-philschatz-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestPhilschatzView from '../lib/test-philschatz-view';
+
+describe('TestPhilschatzView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
